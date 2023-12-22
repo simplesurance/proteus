98887c31702d7fc14510bfbceff4f66813b3c4c3
Francisco Delmar Kurpiel <francisco@kurpiel.eng.br>
2023-12-22 22:15:50 +0100

    feature: adding support for time.Time and time.Duration fields

--- internal/assert/equals.go
+++ internal/assert/equals.go
@@ -24,3 +24,27 @@ func Equal[T comparable](t testing.TB, a, b T) bool {
 	t.Errorf("Values should be equal, but they aren't:\nA: %v\nB: %v", a, b)
 	return false
 }
+
+// EqualNowFn asserts that a.Equals(b).
+func EqualNowFn[T any](t testing.TB, a, b T) {
+	t.Helper()
+
+	if !EqualFn[T](t, a, b) {
+		t.FailNow()
+	}
+}
+
+// EqualFn asserts that "a" is equals to "b" by calling a.Equal(b).
+// Returns true if assertion passes.
+func EqualFn[T any](t testing.TB, a any, b T) bool {
+	t.Helper()
+
+	eqIf := a.(interface{ Equal(other T) bool })
+
+	if eqIf.Equal(b) {
+		return true
+	}
+
+	t.Errorf("Values should be equal, but they aren't:\nA: %v\nB: %v", a, b)
+	return false
+}
